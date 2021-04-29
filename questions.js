dbb9015801330141716cbb7c58ce894b02bd1377
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-04-29 18:00:24 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,103 @@
+//Array of objects
+const quiz = [
+    {
+        q:"When we last visited George, he took us ___ for a drink.",
+        options:["out", "around", "in", "on"],
+        answer: 0
+    },
+    {
+     q:"Take the magazines ___ and put them on the bottom shelf.",
+        options:["off", "away", "down", "out"],
+        answer: 1   
+    },
+    {
+        q:"Alice has just taken ___ the post of the sales manager in Procter & Gamble .",
+        options:["to", "down", "up", "back"],
+        answer: 2
+    },
+    {
+        q:"I always take ___ my shoes and put on my slippers when I get home. ",
+        options:["out", "in", "on", "off"],
+        answer: 3
+    },
+    {
+        q:"Monica took ___ her father as for her character. She's equally stubborn.",
+        options:["after", "over", "up", "around"],
+        answer: 0
+    },
+    {
+        q:"Every year my grandpa takes me ___ the factory he used to work.",
+        options:["up", "around", "after", "off"],
+        answer: 1
+    },
+    {
+        q:"I'll have to take this sweater ___ to the shop because it has shrunk. ",
+        options:["out", "away", "back", "down"],
+        answer: 2
+    },
+    {
+        q:"Sue took ___ my address and told me she would send me a postcard.",
+        options:["out", "on", "to", "down"],
+        answer: 3
+    },
+    {
+        q:"My mum took some of the chairs ___ because I didn't need them. ",
+        options:["away", "back", "out", "on"],
+        answer: 0
+    },
+    {
+        q:"Rachel is exhausted because she has taken ___ too much work.",
+        options:["after", "on", "in", "off"],
+        answer: 1
+    },
+    {
+        q:"Recently Josh has taken ___ jogging.",
+        options:["on", "in", "to", "up"],
+        answer: 2
+    },
+    {
+        q:"We took the new flat ___ a few months ago.",
+        options:["after", "back", "out", "over"],
+        answer: 3
+    },
+    {
+        q:"The secretary took ___ the message.",
+        options:["down", "on", "up", "to"],
+        answer: 0
+    },
+    {
+        q:"The shop assistant told me that I could take the shirt ___ if Mike doesn't like it.",
+        options:["on", "back", "around", "out"],
+        answer: 1
+    },
+    {
+        q:"If I were you, I would take her ___ to dinner.",
+        options:["up", "after", "out", "in"],
+        answer: 2
+    },
+    {
+        q:"The plane was delayed by the fog and took ___ at 10 instead of 9.",
+        options:["in", "out", "away", "off"],
+        answer: 3
+    },
+    {
+        q:"I'm worried how our daughter will take ___ secondary school next year.",
+        options:["to", "after", "out", "in"],
+        answer: 0
+    },
+    {
+        q:"If you don't take the books ___ to the library, they will fine you.",
+        options:["away", "back", "off", "down"],
+        answer: 1
+    },
+    {
+        q:"You'd better take ___ our phone number and don't hesitate to call us when you visit Paris.",
+        options:["on", "up", "down", "out"],
+        answer: 2
+    },
+    {
+        q:"We took ___ Rome on our way back from holyday.",
+        options:["up", "to", "out", "in"],
+        answer: 3
+    }
+]
